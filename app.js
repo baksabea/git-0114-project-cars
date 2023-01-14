46593059bf1bb949c5c451361a7cdad265cb41f3
Baksa Beáta <baksabeaa@gmail.com>
2023-01-14 14:02:48 +0100

    Autók aszinkron megjelenítése oldalbetöltődésre. Ehhez szükséges ág létrehozva és módosítások elvégezve.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const btn = document.getElementById("btn");
+
 const result = document.querySelector(".result");
-btn.addEventListener("click", async () => {
+window.addEventListener("click", async () => {
     const res = await fetch("cars.json");
     const data = await res.json();
     console.log(data);
 
     let output = "";
     data.map(car => {
-        
+
         output += `<div class="car">
         <h2>Márka: ${car.Márka}</h2>
         <h3>Típus: ${car.Típus}</h3>
